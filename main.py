627ec0949c6d9f292de8a998c596a9a6ac455738
Trevin Landers <hello@trevinlanders.com.lk>
2022-07-20 01:17:42 +0530

    Minor Edits

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ root.title('Diabetes Predictor')
 canvas = tk.Canvas(root, height=700, width=700, bg="#7DD3FC")
 canvas.pack()
 
-frame = tk.Frame(root, bg="#BAE6FD")
+frame = tk.Frame(root)
 frame.place(relheight=0.8, relwidth=0.8, relx=0.1, rely=0.1)
 
-Title_label = ttk.Label(root, text="Diabetes Predictor")
-Title_label.place(x=340, y=20)
+Title_label = ttk.Label(root, text="Diabetes Predictor", font='Arial 35')
+Title_label.place(x=150, y=80)
 
 root.mainloop()
